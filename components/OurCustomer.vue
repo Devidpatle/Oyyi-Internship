51a17a23be33279f59b5b6e518c030a1a60d97de
coder9118 <rahulkumar9118@gmail.com>
2022-11-14 16:31:31 +0530

    fix: fixes image import error

--- components/OurCustomer.vue
+++ components/OurCustomer.vue
@@ -8,9 +8,7 @@
           class="flex flex-col sm:flex-row items-center justify-center gap-x-3 mx-5"
       >
         <div class="flex flex-col sm:w-2/3">
-          <h1
-              class="text-center text-gray-100 font-extrabold pb-10 text-3xl md:text-4xl xl:text-5xl text"
-          >
+          <h1 class="text-center text-gray-100 font-extrabold pb-10 text-3xl md:text-4xl xl:text-5xl text">
             We work with the Best Partners
           </h1>
           <p class="text-center text-gray-300 text-sm lg:text-base">
@@ -24,17 +22,17 @@
         >
           <!-- <img src="images/rclogo.png" class="w-64 lg:w-72 grayscale" alt="rc" /> -->
           <img
-              src="/images/logo-suip (1).png"
+              src="public/images/logo-suip.png"
               class="w-auto h-20 xl:h-24 xl:w-auto"
               alt="suipme"
           />
           <img
-              src="/images/lwook.png"
+              src="public/images/lwook.png"
               class="w-20 h-20 xl:h-24 xl:w-24"
               alt="LwookMe"
           />
           <img
-              src="/images/dabfile.png"
+              src="public/images/dabfile.png"
               class="w-20 h-20 xl:h-24 xl:w-24"
               alt="dabfile"
           />
