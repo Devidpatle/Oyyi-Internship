74e7e07a877bf543e053bb989d40ee202786df32
Sarthak <80967858+0xSenpy@users.noreply.github.com>
2022-12-09 22:44:39 +0530

    Update OurCustomer.vue

--- components/OurCustomer.vue
+++ components/OurCustomer.vue
@@ -1,12 +1,12 @@
 <template>
   <section
       id="ourcustomers"
-      class="bg-gray-700 py-24 xl:py-36"
+      class="bg-gray-800 py-24 xl:py-36"
   >
     <div class="container mx-auto">
       <div class="flex flex-col sm:flex-row items-center justify-center gap-x-3 mx-5">
         <div class="flex flex-col sm:w-2/3">
-          <h1 class="text-center text-gray-100 font-extrabold pb-10 text-3xl md:text-4xl xl:text-5xl text">
+          <h1 class="text-center text-green-500 font-extrabold pb-10 text-3xl md:text-4xl xl:text-5xl text">
             We work with the Best Partners
           </h1>
           <p class="text-center text-gray-300 text-sm lg:text-base">
@@ -19,17 +19,17 @@
             class="flex flex-wrap justify-center mx-5 gap-x-8 gap-y-10 py-10 xl:px-10 rounded-3xl bg-opacity-10"
         >
           <img
-              src="public/images/logo-suip.png"
+              src="/images/logo-suip (1).png"
               class="w-auto h-20 xl:h-24 xl:w-auto"
               alt="suipme"
           />
           <img
-              src="public/images/lwook.png"
+              src="/images/lwook.png"
               class="w-20 h-20 xl:h-24 xl:w-24"
               alt="LwookMe"
           />
           <img
-              src="public/images/dabfile.png"
+              src="/images/dabfile.png"
               class="w-20 h-20 xl:h-24 xl:w-24"
               alt="dabfile"
           />
@@ -38,3 +38,4 @@
     </div>
   </section>
 </template>
+
