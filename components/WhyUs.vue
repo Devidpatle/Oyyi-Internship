232827c90f361574492c65f190e6188a44d6cd82
Sarthak <80967858+0xSenpy@users.noreply.github.com>
2022-12-09 22:45:32 +0530

    Update WhyUs.vue

--- components/WhyUs.vue
+++ components/WhyUs.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="feature" class="bg-gray-50">
+  <section id="feature" class="bg-gray-100">
     <div class="container mx-auto">
       <h1 data-aos="fade-up" class="feature_title">Why choose Oyyi?</h1>
       <div class="feature_wrapper">
@@ -11,21 +11,21 @@
         <FeatureCard title="SDKs" description=" Sdk for alm ost every popular programming language like Python , Php , Javascript. (Coming Soon)"/>
       </div>
       <div class="bg-gray-600 bg-opacity-10 py-10 mx-5 rounded-2xl" id="dashboard">
-        <h1 class="block xl:hidden pb-14 text-center font-bold text-3xl lg:text-4xl text-green-500">A dashboard that is made just for you!</h1>
+        <h1 class="block xl:hidden pb-14 text-center font-bold text-3xl lg:text-4xl text-gray-700">A Dashboard that is made just for you!</h1>
         <div class="dashboard_features_row">
           <div class="flex flex-col">
-            <p class="hidden xl:block text-center font-bold text-4xl xl:text-5xl text-green-500">A dashboard that is made just for you!</p>
+            <p class="hidden xl:block text-center font-bold text-4xl xl:text-5xl text-gray-700">A dashboard that is made just for you!</p>
             <div class="dashboard_feature_wrapper">
               <div class="dashboard_features">
-                <img src="https://img.icons8.com/ios/50/A7F3D0/api-settings.png" class="w-7 mx-auto pb-3" alt="manage token"/>
+                <img src="https://img.icons8.com/ios/50/374151/api-settings.png" class="w-7 mx-auto pb-3" alt="manage token"/>
                 <h6>Manage Tokens</h6>
               </div>
               <div class="dashboard_features">
-                <img src="https://img.icons8.com/ios/50/A7F3D0/data-in-both-directions.png" class="w-7 mx-auto pb-3" alt="track response"/>
+                <img src="https://img.icons8.com/ios/50/374151/data-in-both-directions.png" class="w-7 mx-auto pb-3" alt="track response"/>
                 <h6>Track Response</h6>
               </div>
               <div class="dashboard_features">
-                <img src="https://img.icons8.com/ios/50/A7F3D0/bar-chart--v1.png" class="w-7 mx-auto pb-3" alt="view stats"/>
+                <img src="https://img.icons8.com/ios/50/374151/bar-chart--v1.png" class="w-7 mx-auto pb-3" alt="view stats"/>
                 <h6>View Stats</h6>
               </div>
             </div>
@@ -46,7 +46,7 @@ export default {
 </script>
 <style scoped>
 .feature_title {
-  @apply text-center mx-4 text-gray-700 text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-extrabold lg:pt-20 lg:pb-20;
+  @apply text-center py-5 mx-4 text-gray-700 text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-extrabold lg:pt-20 lg:pb-20;
 }
 
 .dashboard_features {
@@ -61,6 +61,6 @@ export default {
   @apply flex sm:flex-col flex-col-reverse lg:flex-row items-center justify-center gap-x-2 lg:mx-20 2xl:mx-40;
 }
 .feature_wrapper{
-  @apply py-16 container flex flex-wrap items-center justify-center gap-x-16 lg:gap-y-6 xl:px-10 mx-auto text-green-600;
+  @apply pt-5 pb-16 container flex flex-wrap items-center justify-center gap-x-10 lg:gap-y-4 xl:px-10 mx-auto text-green-600;
 }
 </style>
