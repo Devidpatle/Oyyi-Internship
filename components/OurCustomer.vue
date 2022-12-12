24e026e9d96b8c3c117e03af8fa05640d04f03d4
Sarthak <80967858+0xSenpy@users.noreply.github.com>
2022-12-12 22:03:58 +0530

    Update OurCustomer.vue

--- components/OurCustomer.vue
+++ components/OurCustomer.vue
@@ -1,15 +1,15 @@
 <template>
   <section
       id="ourcustomers"
-      class="bg-gray-800 py-24 xl:py-36"
+      class="bg-[url('/images/herobg.png')]"
   >
-    <div class="container mx-auto">
-      <div class="flex flex-col sm:flex-row items-center justify-center gap-x-3 mx-5">
+    <div class="mx-auto">
+      <div class="flex bg-white backdrop-blur-xl bg-opacity-50 flex-col sm:flex-row items-center justify-center gap-x-3 py-20 px-3">
         <div class="flex flex-col sm:w-2/3">
-          <h1 class="text-center text-green-500 font-extrabold pb-10 text-3xl md:text-4xl xl:text-5xl text">
+          <h1 class="text-center text-gray-700 pb-10 text-3xl md:text-4xl font-bold">
             We work with the Best Partners
           </h1>
-          <p class="text-center text-gray-300 text-sm lg:text-base">
+          <p class="text-center text-gray-700 text-sm lg:text-base">
             The APIs that we have build are robust and we are confident that
             they can meet all your goals. Its free to begin with and is very
             affordable for all types of businesses and individuals.
@@ -20,17 +20,17 @@
         >
           <img
               src="/images/logo-suip (1).png"
-              class="w-auto h-20 xl:h-24 xl:w-auto"
+              class="w-auto h-16 xl:h-24 xl:w-auto"
               alt="suipme"
           />
           <img
               src="/images/lwook.png"
-              class="w-20 h-20 xl:h-24 xl:w-24"
+              class="w-auto h-16 xl:h-24 xl:w-24"
               alt="LwookMe"
           />
           <img
               src="/images/dabfile.png"
-              class="w-20 h-20 xl:h-24 xl:w-24"
+              class="w-auto h-16 xl:h-24 xl:w-24"
               alt="dabfile"
           />
         </div>
