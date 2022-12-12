d827117af37016f308d9123885a3b35927413810
Sarthak <80967858+0xSenpy@users.noreply.github.com>
2022-12-12 22:01:14 +0530

    Update FeatureCard.vue

--- components/Partials/FeatureCard.vue
+++ components/Partials/FeatureCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-72 h-80 flex flex-col justify-center xl:w-80 my-5 py-5 px-3 bg-gray-200 rounded-3xl">
+  <div class="w-72 h-80 flex flex-col justify-center xl:w-80 my-5 py-5 px-3 bg-gray-100 bg-opacity-60 rounded-3xl">
     <img src="https://img.icons8.com/ios/50/374151/happy-cloud.png" class="mx-auto" :alt="title+'_image'" v-if="title === 'Simple to Use'"/>
     <img src="https://img.icons8.com/ios/50/374151/expensive-2--v1.png" class="mx-auto" :alt="title+'_image'" v-if="title === 'Cost Effective'"/>
     <img src="https://img.icons8.com/ios/50/374151/keyhole-shield.png" class="mx-auto" :alt="title+'_image'" v-if="title === 'Secure'"/>
@@ -8,7 +8,7 @@
     <img src="https://img.icons8.com/ios/50/374151/code.png" class="mx-auto" :alt="title+'_image'" v-if="title === 'SDKs'"/>
 
     <h1 class="text-center text-2xl font-semibold py-4">{{title}}</h1>
-    <p class="text-sm text-gray-500 text-center mt-4 mx-4">
+    <p class="text-sm text-gray-600 text-center mt-4 mx-4">
       {{description}}
     </p>
   </div>
