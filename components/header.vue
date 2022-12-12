ea462f5048045390d4b2f2f7371b5db1094da5f5
Sarthak <80967858+0xSenpy@users.noreply.github.com>
2022-12-12 22:02:55 +0530

    Update header.vue

--- components/header.vue
+++ components/header.vue
@@ -3,12 +3,12 @@
     <div class="container flex justify-between items-center mx-auto px-8 w-full">
       <div class="text-3xl lg:text-4xl font-montserrat font-bold">Oyyi</div>
       <div class="hidden lg:flex space-x-9 items-center font-inter text-base font-medium">
-        <a href="#apis" class="hover:text-green-500">Our APIs</a>
-        <a href="/#feature" class="hover:text-green-500">Features</a>
-        <a href="https://v1-docs.oyyi.xyz/" class="hover:text-green-500">Documentation</a>
-        <a href="/#pricing" class="hover:text-green-500">Pricing</a>
-        <a href="/#dashboard" class="hover:text-green-500">Dashboard</a>
-        <a href="https://dashboard.oyyi.xyz" class="px-4 py-3 bg-green-500 text-zinc-900 rounded-lg hover:bg-emerald-300">Sign Up</a
+        <a href="#apis" class="hover:text-teal-500">Our APIs</a>
+        <a href="/#feature" class="hover:text-teal-500">Features</a>
+        <a href="https://v1-docs.oyyi.xyz/" class="hover:text-teal-500">Documentation</a>
+        <a href="/#pricing" class="hover:text-teal-500">Pricing</a>
+        <a href="/#dashboard" class="hover:text-teal-500">Dashboard</a>
+        <a href="https://dashboard.oyyi.xyz" class="px-4 py-3 bg-primary text-zinc-900 rounded-lg hover:bg-teal-500">Sign Up</a
         >
       </div>
         <button class="outline-none mobile-menu-button hidden">
