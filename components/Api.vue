eaea2e9657f4934d3cad918fec50a4e43f5198cb
Sarthak <80967858+0xSenpy@users.noreply.github.com>
2022-12-14 12:09:55 +0530

    Update Api.vue

--- components/Api.vue
+++ components/Api.vue
@@ -4,13 +4,13 @@
       <h1 data-aos="fade-up" class="api_title">We Can Meet All Your Needs </h1>
       
       <div id="media-api" class="mx-5 my-10 py-5 px-5 bg-[url('/images/mediabg.png')] rounded-2xl">
-        <div class="flex bg-white backdrop-blur-xl py-16 bg-opacity-80 rounded-2xl flex-col lg:flex-row lg:items-center lg:justify-center">
+        <div class="card_inner">
           <!-- <div class="w-full lg:w-2/3">
           <img src="/images/media.png" class="h-55 mx-auto" alt="Image Processing api"/>
           </div> -->
           <div class="flex flex-col">
-            <h1 class="media_api_title">Media APIs</h1>
-            <p class="media_description">
+            <h1 class="api_type_title">Media APIs</h1>
+            <p class="api_type_desc">
               Image Conversion through API or on the fly with web request.
               Almost all type of image manipulation like crop, resize, blur,
               sharp and many more can be done with just one request.
@@ -22,13 +22,13 @@
         </div>
       </div>
       <div id="machine-learning-api" class="mx-5 my-10 py-5 px-5 bg-[url('/images/mlbg.png')] rounded-2xl">
-        <div class="flex bg-white backdrop-blur-xl py-16 bg-opacity-80 rounded-2xl flex-col lg:flex-row lg:items-center lg:justify-center">
+        <div class="card_inner">
           <!-- <div class="w-full lg:w-2/3">
           <img src="/images/machinelearning.png" class="h-55 mx-auto" alt="Machine Learning API"/>
           </div> -->
           <div class="flex flex-col">
-            <h1 class="media_api_title">Machine Learning APIs</h1>
-            <p class="media_description">
+            <h1 class="api_type_title">Machine Learning APIs</h1>
+            <p class="api_type_desc">
               Image Conversion through API or on the fly with web request.
               Almost all type of image manipulation like crop, resize, blur,
               sharp and many more can be done with just one request.
@@ -40,13 +40,13 @@
         </div>
       </div>
       <div id="general-api" class="mx-5 my-10 py-5 px-5 bg-[url('/images/generalbg.png')] rounded-2xl">
-        <div class="flex bg-white backdrop-blur-xl py-16 bg-opacity-80 rounded-2xl flex-col lg:flex-row lg:items-center lg:justify-center">
+        <div class="card_inner">
           <!-- <div class="w-full lg:w-2/3">
           <img src="/images/general.png" class="h-42 mx-auto" alt="General Purpose API"/>
           </div> -->
           <div class="flex flex-col">
-            <h1 class="media_api_title">General Purpose APIs</h1>
-            <p class="media_description">
+            <h1 class="api_type_title">General Purpose APIs</h1>
+            <p class="api_type_desc">
               Image Conversion through API or on the fly with web request.
               Almost all type of image manipulation like crop, resize, blur,
               sharp and many more can be done with just one request.
@@ -65,14 +65,11 @@
 
 <style scoped>
 
-.media_description{
+.api_type_desc{
   @apply text-sm text-center font-inter font-light text-gray-800 mt-4 mx-5;
 }
-.media_api_title{
+.api_type_title{
   @apply text-center text-gray-700 font-montserrat text-3xl lg:text-4xl mx-5 font-semibold;
-}
-.apis_card_wrapper{
-  @apply mt-20 px-5 py-10 xl:px-10 container flex flex-wrap items-center justify-center gap-x-16 gap-y-10 mx-auto;
 }
 .read_btn {
   @apply mx-auto mt-6 py-2 text-center font-semibold text-sm border-2 border-gray-700 bg-white bg-opacity-40 border-opacity-20 rounded-lg font-poppins text-primary hover:text-emerald-700;
@@ -80,7 +77,8 @@
 .api_title{
   @apply text-center text-gray-700 mx-4 py-5 text-3xl sm:text-4xl lg:text-5xl font-inter font-bold lg:pt-24 lg:pb-24;
 }
-.api-desc {
-  @apply text-sm h-20 font-inter text-gray-400 text-center mx-4;
+.card_inner{
+  @apply flex bg-white backdrop-blur-xl py-16 bg-opacity-80 rounded-2xl flex-col lg:flex-row lg:items-center lg:justify-center
 }
+
 </style>
