c28e68740b3bd76b05e491c958b1f4fc06902dd1
coder9118 <rahulkumar9118@gmail.com>
2023-02-11 13:43:01 +0530

    fix: fixes to pull latest image from repo

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,8 +5,7 @@
       <div class="row">
         <div class="col-lg-8 col-md-12 mx-auto text-center">
           <h1 class="display-1 fw-bolder my-3">Cloud Processing Made Out Of Perfection!</h1>
-          <p class="lead"> Our multiple APIs are made to meet your need as developer and implement without getting worried about coding it
-            yourself.</p>
+          <p class="lead"> Our multiple APIs are made to meet your need as developer and implement without getting worried about coding it.</p>
           <div class="mt-5 mx-auto d-flex">
             <a href="https://dashboard.oyyi.xyz" class="btn btn-secondary shadow-none border-0 rounded-0 flex-grow-1 py-3 px-5 btn-lg row-gap-2">Get
               Started</a>&nbsp;&nbsp;
