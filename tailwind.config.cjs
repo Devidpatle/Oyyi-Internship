18560bd3ff3c7ec41079b2b7ea34bbdde585c298
coder9118 <rahulkumar9118@gmail.com>
2023-03-24 18:59:26 +0530

    fix: modified primary theme of website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [require("daisyui")],
+	daisyui:{
+		themes: [
+			{
+				oyyi_theme: {
+					primary: "#1f7a8c",
+					secondary: "#222E50",
+					accent: "#B80C09",
+					neutral: "#3d4451",
+					"base-100": "#ffffff",
+				},
+			},
+		],
+	}
 }
